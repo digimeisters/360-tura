a935033264847781979b3ca06d1e3262e8e0e3e4
Digimeister <nikola.stoyanov.kgb@gmail.com>
2026-08-30 00:12:06 +0200

    fix supabase key variable name drugi

--- app/api/ai/process-form/route.ts
+++ app/api/ai/process-form/route.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY // process.env.SUPABASE_ANON_KEY! 
+  process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_ANON_KEY! 
 );
 
 export async function POST(req: Request) {
@@ -12,8 +12,8 @@ export async function POST(req: Request) {
     console.log("PRIMLJENO IZ FORME:", JSON.stringify(body));
 
     // Fleksibilno izvlačenje podataka (hvata različite formate iz App Script-a 
-        const slug = body.slug || body.parameter?.slug //"nepoznato";
-        const answers =body.answers //body.namedValues //body;
+        const slug = body.slug || body.parameter?.slug ||"nepoznato";
+        const answers = body.answers ||body.namedValues ||body;
 
     // Upis u Supabase tabelu (promeni 'tvoja_tabela' u naziv tvoje tabele)
     const { data, error } = await supabase
