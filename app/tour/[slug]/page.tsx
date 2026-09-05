84fa28e6556c5cdc292fcb0a7a10bb40dab344c9
Digimeister <nikola.stoyanov.kgb@gmail.com>
2026-09-05 17:26:16 +0200

    pokusaj 2 za manipulaciju tacaka

--- app/tour/[slug]/page.tsx
+++ app/tour/[slug]/page.tsx
@@ -850,7 +850,7 @@ export default function TourPage() {
         waypoints_i18n: updatedWaypoints,
         establish_i18n: updatedEstablish
       })
-      .eq('id', currentRoom.id);
+      .eq('id', currentRoom.id as any);
 
     if (dbErr) throw dbErr;
 
@@ -887,7 +887,7 @@ export default function TourPage() {
       const { error: dbErr } = await supabase
         .from('rooms')
         .update({ waypoints_i18n: updatedWaypoints })
-        .eq('id', currentRoom.id);
+        .eq('id', currentRoom.id as any);
 
       if (dbErr) throw dbErr;
 
@@ -1021,7 +1021,7 @@ export default function TourPage() {
           establish_i18n: currentEstablishI18n,
           waypoints_i18n: currentWaypoints
         })
-        .eq('id', currentRoom.id);
+        .eq('id', currentRoom.id as any);
 
       if (dbErr) {
         throw dbErr;
